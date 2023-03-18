07d204e66e2c5494e4d6752d8d137e709b4f1f47
kadiiravsaar <kadiiravsar@gmail.com>
2023-03-18 17:34:03 +0300

    Koleksiyonlar Added

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Koleksiyonlar;
+
+List<string> cities = new List<string>(); // Ben bir koleksiyon oluşturmak istiyorum dizi temelli olsun ve sadece string ile çalışabiliyorum
+
+
+//List<Customer> customers = new List<Customer>();
+//customers.Add(new Customer { Id = 2, Name = "Kadir" });
+
+List<Customer> customers = new List<Customer>()
+{
+    new Customer {Id=1,Name ="Kadir"},
+    new Customer {Id=2,Name ="Ali"}
+};
+
+var count = customers.Count();
+
+foreach (var cst in customers)
+{
+    Console.WriteLine(cst.Name);
+}
